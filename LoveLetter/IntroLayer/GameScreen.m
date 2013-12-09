293868cf22621e77cc7aa149dfe8d93308f037f5
admiralronton <rsouthwick@Ryans-MacBook-Pro.local>
2013-12-09 13:46:36 -0800

    Moved draw deck

--- LoveLetter/IntroLayer/GameScreen.m
+++ LoveLetter/IntroLayer/GameScreen.m
@@ -146,8 +146,8 @@
     {
         CCSprite* card = [Deck getBackCardSprite];
         card.scale = 0.2f;
-        float pointX = self.contentSize.width - (card.contentSize.width * card.scale / 2.0f) - 20.0f + ((float)i * 5.0f);
-        float pointY = (card.contentSize.height * card.scale / 2.0f) + 20.0f - ((float)i * 5.0f);
+        float pointX = self.contentSize.width - (card.contentSize.width * card.scale / 2.0f) - 35.0f + ((float)i * 5.0f);
+        float pointY = (card.contentSize.height * card.scale / 2.0f) + 35.0f - ((float)i * 5.0f);
         CGPoint cardPos = ccp(pointX, pointY);
         card.position = cardPos;
         
@@ -161,8 +161,8 @@
     float cardCountScale = 2.0f;
     [drawDeckCount setString:[NSString stringWithFormat:@"%i", cardCount]];
     drawDeckCount.scale = cardCountScale;
-    float pointX = self.contentSize.width - (drawDeckCount.contentSize.width * drawDeckCount.scale / 2.0f) - 40.0f;
-    float pointY = (drawDeckCount.contentSize.height * drawDeckCount.scale / 2.0f) + 20.0f;
+    float pointX = self.contentSize.width - (drawDeckCount.contentSize.width * drawDeckCount.scale / 2.0f) - 55.0f;
+    float pointY = (drawDeckCount.contentSize.height * drawDeckCount.scale / 2.0f) + 35.0f;
     CGPoint countPos = ccp(pointX, pointY);
     
     drawDeckCount.position = countPos;
